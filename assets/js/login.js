e4bd23640a5266aa578a84e621b1f5d3fe0d8009
derlycanon <derlycaon@gmail.com>
2022-05-11 16:27:16 -0500

    Add actualizations with js

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,10 +5,10 @@
 
 
 let bingresa = document.getElementById("bingresa");
-let modal_primary = document.getElementById("modal_primary");
-
+if(bingresa != null){ 
     bingresa.addEventListener("click", () => {
-        let user = document.getElementById("user").value;
+        let modal_primary = document.getElementById("modal_primary");
+        let user = document.getElementById ("user").value;
         let pass = document.getElementById("pass").value;
         if(user === "Admin" && pass === "Kuepa2022"){
            window.location = "dashboard.html";
@@ -22,4 +22,40 @@ let mpBoton = document.getElementById("mpBoton");
     mpBoton.addEventListener("click",() => {
         modal_primary.style.display = "none";
  });
+}
 
+//interacción pacientes - Dashboard
+let form=document.getElementById("formConsul");
+
+form.addEventListener("submit",(e)=>{
+
+    var consul = parseInt(document.getElementById("consulta").value);
+    console.log(consul);
+    e.preventDefault();
+
+    if(consul == 52368795){
+        let userOne=document.getElementById("userOne");
+        userOne.style.setProperty("display","block");
+        let continuar = document.getElementById("continuar")
+        continuar.addEventListener("click", ()=>{
+        window.location="registro.html";}
+        )} 
+
+    else if (consul == 1001235478){
+        let userTwo=document.getElementById("userTwo");
+        userTwo.style.setProperty("display","block");
+        let continuar = document.getElementById("continuar")
+        continuar.addEventListener("click", ()=>{
+        window.location="registro.html";}
+        )} 
+
+    
+    else if (consul == 25413574){
+        let userThree=document.getElementById("userThree");
+        userThree.style.setProperty("display","block");
+    }
+    else if
+        (consul == 1007203506){
+        let userFour=document.getElementById("userFour");
+        userFour.style.setProperty("display","block");
+    }});
